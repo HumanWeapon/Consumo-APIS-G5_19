e26a532ec9575da653d6419077dbcc6be00b7424
Angelvas <anvasquez@unah.hn>
2023-04-18 04:11:06 -0600

    Carga de entidad banco Insert

--- JS/Banco.js
+++ JS/Banco.js
@@ -1,4 +1,5 @@
 var UrlApiGetAll = 'http://localhost:5005/banco/getall';
+var UrlApiInsert = 'http://localhost:5005/banco/insertar';
 
 $(document).ready( function(){
     CargarBancos();
@@ -15,16 +16,45 @@ function CargarBancos(){
             for(i=0; i<MissItems.length; i++){
                 Valores +=
                 '<tr>'+
-                    '<td>' + MissItems[i].od_banco + '</td>' +
+                    '<td>' + MissItems[i].cod_banco + '</td>' +
                     '<td>' + MissItems[i].nombre + '</td>' +
                     '<td>' + MissItems[i].of_principal + '</td>' +
                     '<td>' + MissItems[i].cant_sucursales + '</td>' +
                     '<td>' + MissItems[i].fch_fundacion + '</td>' +
                     '<td>' + MissItems[i].pais + '</td>' +
                     '<td>' + MissItems[i].rtn + '</td>' +
+                    '<td>' +
+                        '<button id="btnedir" class="btn btn-dark" onclick="AgregarBanco()">Editar</button>' +
+                    '</td>' +
                 '</tr>';
                 $('#DatosBancos').html(Valores);
             }
         } 
+    });
+}
+function AgregarBanco(){
+    var DatosBanco = {
+        Cod_Banco: $('#No_Banco').val(),
+        Nombre: $('#Nombre').val(),
+        OF_Principal: $('#OfPrincipal').val(),
+        Cant_Sucursales: $('#CantSucursales').val(),
+        Fch_Fundacion: $('#Fech_Fundacion').val(),
+        Pais: $('#Pais').val(),
+        RTN: $('#RTN').val()
+    };
+    var DatosBancoJSON = JSON.stringify(DatosBanco);
+    $.ajax({
+        url: UrlApiInsert,
+        type: 'POST',
+        data: DatosBancoJSON,
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function(response){
+            alert('Banco ingresado de forma correcta')
+            $('#MiFormulario').submit();
+        },
+        error: function(textError, errorThrown){
+            alert('Error: '+ textError+errorThrown);
+        }
     });
 }
