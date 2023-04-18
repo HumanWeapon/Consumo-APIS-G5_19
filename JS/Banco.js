01b72b4481c67656fb6617c4204286c2c9eff7a9
Angelvas <anvasquez@unah.hn>
2023-04-18 07:31:48 -0600

    Carga de entidad BANCO, INSERT y GET ONE, Carga de INFORMACIÖN y HOME

--- JS/Banco.js
+++ JS/Banco.js
@@ -1,5 +1,6 @@
 var UrlApiGetAll = 'http://localhost:5005/banco/getall';
 var UrlApiInsert = 'http://localhost:5005/banco/insertar';
+var UrlApiGetOne = 'http://localhost:5005/banco/getone/:cod_banco';
 
 $(document).ready( function(){
     CargarBancos();
@@ -12,7 +13,7 @@ function CargarBancos(){
         datatype: 'JSON',
         success: function(response){
             var MissItems = response;
-            var Valores = '';;
+            var Valores = '';
             for(i=0; i<MissItems.length; i++){
                 Valores +=
                 '<tr>'+
@@ -24,10 +25,11 @@ function CargarBancos(){
                     '<td>' + MissItems[i].pais + '</td>' +
                     '<td>' + MissItems[i].rtn + '</td>' +
                     '<td>' +
-                        '<button id="btnedir" class="btn btn-dark" onclick="AgregarBanco()">Editar</button>' +
+                        '<button id="btneditar" class="btn btn-dark" onclick="CargarBanco('+ MissItems[i].cod_banco +')">Editar</button>' +
                     '</td>' +
                 '</tr>';
                 $('#DatosBancos').html(Valores);
+                
             }
         } 
     });
@@ -54,7 +56,43 @@ function AgregarBanco(){
             $('#MiFormulario').submit();
         },
         error: function(textError, errorThrown){
-            alert('Error: '+ textError+errorThrown);
+            alert('Error: '+ textError + errorThrown);
         }
     });
+}
+function CargarBanco(P_cod_banco){
+
+    var DatosBanco = {
+        cod_banco: P_cod_banco
+    };
+    var datosBancojson = JSON.stringify(DatosBanco);
+    $.ajax({
+        url: UrlApiGetOne,
+        type: 'POST',
+        data: datosBancojson,
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function(response){
+            var Item = response;
+
+            for(i=0; i<Item.length; i++){
+                $('#No_Banco').val(Item[i].cod_banco);
+                $('#Nombre').val(Item[i].nombre);
+                $('#OfPrincipal').val(Item[i].of_principal);
+                $('#CantSucursales').val(Item[i].cant_sucursales);
+                $('#Fech_Fundacion').val(Item[i].fch_fundacion);
+                $('#Pais').val(Item[i].pais);
+                $('#RTN').val(Item[i].rtn);
+                var btnActualizar = '<input type="submit" value="Actualizar Banco" class="btn btn-success" id="btnActualizar" onclick="ActualizarBanco('+ Item[i].cod_banco +')">';
+                $('#btnAgregar').html(btnActualizar);
+
+            }
+        },
+        error: function(textError, errorThrown){
+            alert('Error: '+ textError + errorThrown);
+        }
+    });
+}
+function ActualizarBanco(P_cod_banco){
+
 }
